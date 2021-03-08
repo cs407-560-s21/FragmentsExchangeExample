a027d6d5bc31f5c1ae655b927af2b085d3267e43
CS416F19 <cs416f19@gmail.com>
2021-03-08 13:59:56 -0500

    Initial commit

---
+++ app/src/main/java/com/example/fragmentsexchangeexample/MainActivity.kt
@@ -0,0 +1,41 @@
+package com.example.fragmentsexchangeexample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class MainActivity : AppCompatActivity() {
+
+    // A global variable to alternate fragments
+    private var isFragmentOneLoaded = false
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    // This function replaces one fragment with another when the button is clicked
+    fun exchangeFragments(view: View) {
+
+        // Begin the transaction
+        val transaction = supportFragmentManager.beginTransaction()
+
+        if (isFragmentOneLoaded) {
+            transaction.replace(R.id.fragment1, FragmentOne())
+            transaction.replace(R.id.fragment2, FragmentTwo())
+            isFragmentOneLoaded = false
+        }
+        else {
+            transaction.replace(R.id.fragment1, FragmentTwo())
+            transaction.replace(R.id.fragment2, FragmentOne())
+            isFragmentOneLoaded = true
+        }
+
+        // This might be used to add the fragments to backstack so that the user may go back the previous state with back button
+        //transaction.addToBackStack(null)
+
+        // Commit the transaction to apply
+        transaction.commit()
+
+    }
+}
